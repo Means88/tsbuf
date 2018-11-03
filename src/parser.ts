b515bd0463c07f97791f0b5bd385aafd39635d5b
Means88 <means88.x@gmail.com>
2018-11-03 19:36:02 +0800

    stash

--- src/parser.ts
+++ src/parser.ts
@@ -24,6 +24,7 @@ import {
   Identifier,
   ImportStatement,
   IntegerLiteral,
+  MessageMember,
   OptionStatement,
   PackageDeclaration,
   StringLiteral,
@@ -165,6 +166,10 @@ export class ProtobufParser extends Parser {
         };
       },
     );
+
+    // p.RULE('messageMember', (): MessageMember => {
+    //
+    // });
 
     // "string"
     p.RULE(
--- src/token/keywords.ts
+++ src/token/keywords.ts
@@ -46,3 +46,121 @@ export const EnumToken = c({
   name: 'EnumToken',
   pattern: /\benum\b/,
 });
+
+const FieldRuleKeywordToken = createToken({
+  name: 'FieldRuleKeywordToken',
+  pattern: Lexer.NA,
+});
+
+export const RepeatedToken = c({
+  name: 'RepeatedToken',
+  pattern: '\brepeated\b',
+  categories: [FieldRuleKeywordToken],
+});
+
+export const RequiredToken = c({
+  name: 'RequiredToken',
+  pattern: '\brequired\b',
+  categories: [FieldRuleKeywordToken],
+});
+
+export const OptionalToken = c({
+  name: 'OptionalToken',
+  pattern: '\boptional\b',
+  categories: [FieldRuleKeywordToken],
+});
+
+export const DefaultToken = c({
+  name: 'DefaultToken',
+  pattern: '\bdefault\b',
+});
+
+// types
+export const KeywordTypeToken = c({
+  name: 'KeywordTypeToken',
+  pattern: Lexer.NA,
+});
+
+function d(config: ITokenConfig): TokenType {
+  const token = c(config);
+  if (!token.CATEGORIES) {
+    token.CATEGORIES = [];
+  }
+  token.CATEGORIES.push(KeywordTypeToken);
+  return token;
+}
+
+export const DoubleToken = d({
+  name: 'DoubleToken',
+  pattern: /\bdouble\b/,
+});
+
+export const FloatToken = d({
+  name: 'FloatToken',
+  pattern: /\bfloat\b/,
+});
+
+export const Int32Token = d({
+  name: 'Int32Token',
+  pattern: /\bint32\b/,
+});
+
+export const Int64Token = d({
+  name: 'Int64Token',
+  pattern: /\bint64\b/,
+});
+
+export const UInt32Token = d({
+  name: 'UInt32Token',
+  pattern: /\buint32\b/,
+});
+
+export const UInt64Token = d({
+  name: 'UInt64Token',
+  pattern: /\buint64\b/,
+});
+
+export const SInt32Token = d({
+  name: 'SInt32Token',
+  pattern: /\bsint32\b/,
+});
+
+export const SInt64Token = d({
+  name: 'SInt64Token',
+  pattern: /\bsint64\b/,
+});
+
+export const Fixed32Token = d({
+  name: 'Fixed32Token',
+  pattern: /\bfixed32\b/,
+});
+
+export const Fixed64Token = d({
+  name: 'Fixed64Token',
+  pattern: /\bfixed64\b/,
+});
+
+export const SFixed32Token = d({
+  name: 'SFixed32Token',
+  pattern: /\bsfixed32\b/,
+});
+
+export const SFixed64Token = d({
+  name: 'SFixed64Token',
+  pattern: /\bsfixed64\b/,
+});
+
+export const BoolToken = d({
+  name: 'BoolToken',
+  pattern: /\bbool\b/,
+});
+
+export const StringToken = d({
+  name: 'StringToken',
+  pattern: /\bstring\b/,
+});
+
+export const BytesToken = d({
+  name: 'BytesToken',
+  pattern: /\bbytes\b/,
+});
